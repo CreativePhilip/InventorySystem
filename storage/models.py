6a0cdd82f08db384cf39c8c7629113b7893867f5
philip <creativepyt@gmail.com>
2020-07-09 17:27:20 +0200

    basic models

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class StorageUnit(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    row_count = models.IntegerField()
+    column_count = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class StorageItem(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    row_index = models.IntegerField()
+    column_index = models.IntegerField()
+
+    storage_unit = models.ForeignKey(StorageUnit, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
